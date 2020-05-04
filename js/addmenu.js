9f0cc996e4bceeba5164084fb1e5869d83cfff9e
sophie-turok <64563245+sophie-turok@users.noreply.github.com>
2020-05-04 09:49:05 -0400

    Update addmenu.js

--- js/addmenu.js
+++ js/addmenu.js
@@ -20,8 +20,8 @@ projects.id = 'boldlink'
 mainmenu.appendChild(projects)
 
 // EDIT THESE TWO LINES TO ADD A PAGE
-var projectlinks = ['LittlePilesEverywhere','domesticinstallations','Signs','wordblocks','pants','youhavetheseshelveswhatwouldyouputonthem','yardsigns','free']
-var projecttitles = ['Little Piles Everywhere', 'domestic installations','Signs', 'wordblocks', 'pants', 'You have these shelves what would you put on them', 'yard signs', 'free']
+var projectlinks = ['LittlePilesEverywhere','gonetothedogs','Signs','wordblocks','pants','youhavetheseshelveswhatwouldyouputonthem','yardsigns','free']
+var projecttitles = ['Little Piles Everywhere', 'gone to the dogs','Signs', 'wordblocks', 'pants', 'You have these shelves what would you put on them', 'yard signs', 'free']
 projectlinks.forEach((projectlink, index) => {
 	var button = document.createElement('a')
 	button.innerHTML = projecttitles[index]
